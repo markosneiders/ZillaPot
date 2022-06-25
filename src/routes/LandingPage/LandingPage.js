712785dcd6fd9a06031a6092e40e61d35bd9ed64
Roberts Mezals <roberts.mezals@gmail.com>
2022-06-25 14:42:20 +0300

    Even more landing page improvements

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,16 +1,24 @@
 import { useNavigate } from "react-router-dom"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./LandingPage.css"
 
 //Component imports
 import ConnectButton from "../../components/ConnectButton/ConnectButton"
 
 //Package imports
-// import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown"
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown"
+import ScrollAnimation from "react-animate-on-scroll"
+import { useSpring, animated } from "react-spring"
 // const { Zilliqa } = require("@zilliqa-js/zilliqa")
 // const zilliqa = new Zilliqa("https://dev-api.zilliqa.com")
 
 function LandingPage() {
+    const fade = useSpring({
+        to: { opacity: 1 },
+        from: { opacity: 0 },
+        config: { duration: 1000 },
+    })
+
     const navigate = useNavigate()
 
     function checkWallet() {
@@ -38,17 +46,78 @@ function LandingPage() {
     return (
         <div
             className="LandingPage"
-            style={{ height: window.innerHeight, width: window.innerWidth }}
+            style={{ height: window.innerHeight * 2, width: window.innerWidth }}
         >
-            <h1 className="LandingPage__title">ZillaPot</h1>
-            <h1 className="LandingPage__text">
-                First sunk cost game on the Zilliqa chain
-            </h1>
-            <ConnectButton
-                onClick={() => {
-                    handleConnect()
+            <animated.div
+                className="LandingPage__main"
+                style={{
+                    height: window.innerHeight,
+                    width: window.innerWidth,
+                    ...fade,
                 }}
-            />
+            >
+                <h1 className="LandingPage__main-title">ZillaPot</h1>
+                <h1 className="LandingPage__main-text">
+                    First sunk cost game on the Zilliqa chain
+                </h1>
+                <ConnectButton
+                    onClick={() => {
+                        handleConnect()
+                    }}
+                />
+                <div className="LandingPage__learnMore">
+                    <h1 className="LandingPage__learnMore-text">Learn more</h1>
+                    <KeyboardDoubleArrowDownIcon fontSize="large" />
+                </div>
+            </animated.div>
+            <div
+                className="LandingPage__main"
+                style={{ height: window.innerHeight, width: window.innerWidth }}
+            >
+                <ScrollAnimation animateIn="fadeIn">
+                    <div className="LandingPage__main__cards">
+                        <div className="LandingPage__main__cards-cardBase LandingPage__main__cards-card1">
+                            <h1 className="LandingPage__main__cards-card-title">
+                                First of it's kind
+                            </h1>
+                            <h1 className="LandingPage__main__cards-card-text">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat.
+                            </h1>
+                        </div>
+                        <div className="LandingPage__main__cards-cardBase LandingPage__main__cards-card2">
+                            <h1 className="LandingPage__main__cards-card-title">
+                                First of it's kind
+                            </h1>
+                            <h1 className="LandingPage__main__cards-card-text">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat.
+                            </h1>
+                        </div>
+                        <div className="LandingPage__main__cards-cardBase LandingPage__main__cards-card3">
+                            <h1 className="LandingPage__main__cards-card-title">
+                                First of it's kind
+                            </h1>
+                            <h1 className="LandingPage__main__cards-card-text">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat.
+                            </h1>
+                        </div>
+                    </div>
+                </ScrollAnimation>
+            </div>
         </div>
     )
 }
