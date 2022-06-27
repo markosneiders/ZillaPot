7c29f73a66c3a6aadc81c945a43a2681388e6062
Roberts Mezals <roberts.mezals@gmail.com>
2022-06-27 15:13:13 +0300

    Profile page done

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,7 +2,10 @@ import "./Container.css"
 
 function Container(props) {
     return (
-        <div className="Container" style={{ height: props.height }}>
+        <div
+            className="Container"
+            style={{ height: props.height, width: props.width }}
+        >
             <div className="Container__header">
                 <h1 className="Container__header-title">
                     {props.headerTitleLeft}
--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -11,10 +11,6 @@ function Profile() {
         balance: "",
     })
 
-    const [dark, setDark] = React.useState(
-        localStorage.getItem("dark-mode") === "true"
-    )
-
     const winnerData = {
         id: 12,
         potSize: 12,
@@ -28,30 +24,11 @@ function Profile() {
         potSize: 20,
     }
     async function getUserData() {
-        console.log(window.zilpay.wallet)
-        const zilliqa = window.zilPay
-        const address = zilliqa.wallet.defaultAccount.bech32
-        setUserData({ address: address })
+        console.log(window.zilPay)
     }
-    // useEffect(() => {
-    //     if (typeof window.zilPay !== "undefined") {
-    //         // ZilPay user detected. You can now use the provider.
-    //         setUserData({ address: window.zilPay.wallet.defaultAccount.bech32 })
-    //     }
-    // }, [window.zilPay])
-    // useEffect(() => {
-    //     test(window)
-    // }, [])
-
-    // function test(a) {
-    //     console.log(a)
-    //     console.log(a.zilPay)
-    //     console.log(a)
-    // }
-
-    React.useEffect(() => {
-        localStorage.setItem("dark-mode", dark)
-    }, [dark])
+    useEffect(() => {
+        getUserData()
+    }, [])
     return (
         <div
             className="Profile"
@@ -62,113 +39,119 @@ function Profile() {
             </div>
             <div className="Profile__container">
                 <Container
+                    width={"fit-content"}
+                    height={"fit-content"}
                     headerTitleLeft="Profile"
                     headerTitleRight={`${userData.address.slice(
                         0,
                         6
                     )}...${userData.address.slice(-6)}`}
-                    content={<div></div>}
-                />
-                <div className="Profile__container-cards">
-                    <div className="Pot">
-                        <div className="Pot__header">
-                            <h3 className="Pot__header-text">{`#${regularData.id}`}</h3>
-                            <h3 className="Pot__header-text">Joined</h3>
-                        </div>
-                        <div className="Pot__body">
-                            <h3
-                                className="Pot__body-title"
-                                style={{ fontSize: 40 }}
-                            >
-                                You have joined
-                            </h3>
-                            <h1
-                                className="Pot__body-title"
-                                style={{ fontSize: "50px" }}
-                            >
-                                {regularData.potSize}
-                            </h1>
-                            <h3
-                                className="Pot__body-text"
-                                style={{ fontWeight: 100, margin: 0 }}
-                            >
-                                pots
-                            </h3>
-                        </div>
-                    </div>
-                    <div className="WPot">
-                        <div className="WPot__header">
-                            <h3 className="WPot__header-text">{`#${winnerData.id}`}</h3>
-                            <h3 className="WPot__header-text">Won</h3>
-                        </div>
-                        <div className="WPot__body">
-                            <div style={{ display: "flex" }}>
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                    }}
-                                >
-                                    <h1
-                                        className="WPot__body-title"
-                                        style={{ fontSize: "45px" }}
+                    content={
+                        <div className="Profile__container-cards">
+                            <div className="Pot" style={{ cursor: "default" }}>
+                                <div className="Pot__header">
+                                    <h3 className="Pot__header-text">{`#${regularData.id}`}</h3>
+                                    <h3 className="Pot__header-text">Joined</h3>
+                                </div>
+                                <div className="Pot__body">
+                                    <h3
+                                        className="Pot__body-title"
+                                        style={{ fontSize: 40 }}
                                     >
-                                        You have won
+                                        You have joined
+                                    </h3>
+                                    <h1
+                                        className="Pot__body-title"
+                                        style={{ fontSize: "50px" }}
+                                    >
+                                        {regularData.potSize}
                                     </h1>
+                                    <h3
+                                        className="Pot__body-text"
+                                        style={{ fontWeight: 100, margin: 0 }}
+                                    >
+                                        pots
+                                    </h3>
                                 </div>
                             </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <h1
-                                    className="WPot__body-title"
-                                    style={{ fontSize: "50px" }}
-                                >
-                                    {winnerData.potSize}
-                                </h1>
-                                <h3
-                                    className="WPot__body-text"
-                                    style={{ fontWeight: 100, margin: 0 }}
-                                >
-                                    times
-                                </h3>
+                            <div className="WPot" style={{ cursor: "default" }}>
+                                <div className="WPot__header">
+                                    <h3 className="WPot__header-text">{`#${winnerData.id}`}</h3>
+                                    <h3 className="WPot__header-text">Won</h3>
+                                </div>
+                                <div className="WPot__body">
+                                    <div style={{ display: "flex" }}>
+                                        <div
+                                            style={{
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                            }}
+                                        >
+                                            <h1
+                                                className="WPot__body-title"
+                                                style={{ fontSize: "45px" }}
+                                            >
+                                                You have won
+                                            </h1>
+                                        </div>
+                                    </div>
+                                    <div
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        <h1
+                                            className="WPot__body-title"
+                                            style={{ fontSize: "50px" }}
+                                        >
+                                            {winnerData.potSize}
+                                        </h1>
+                                        <h3
+                                            className="WPot__body-text"
+                                            style={{
+                                                fontWeight: 100,
+                                                margin: 0,
+                                            }}
+                                        >
+                                            times
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="LPot" style={{ cursor: "default" }}>
+                                <div className="LPot__header">
+                                    <h3 className="LPot__header-text">{`#${loserData.id}`}</h3>
+                                    <h3 className="LPot__header-text">Lost</h3>
+                                </div>
+                                <div className="LPot__body">
+                                    <h1
+                                        className="LPot__body-title"
+                                        style={{ fontSize: "45px" }}
+                                    >
+                                        You have lost
+                                    </h1>
+                                    <h1
+                                        className="LPot__body-title"
+                                        style={{ fontSize: "50px" }}
+                                    >
+                                        {loserData.potSize}
+                                    </h1>
+                                    <h3
+                                        className="LPot__body-text"
+                                        style={{ fontWeight: 100, margin: 0 }}
+                                    >
+                                        times
+                                    </h3>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="LPot">
-                        <div className="LPot__header">
-                            <h3 className="LPot__header-text">{`#${loserData.id}`}</h3>
-                            <h3 className="LPot__header-text">Lost</h3>
-                        </div>
-                        <div className="LPot__body">
-                            <h1
-                                className="LPot__body-title"
-                                style={{ fontSize: "45px" }}
-                            >
-                                You have lost
-                            </h1>
-                            <h1
-                                className="LPot__body-title"
-                                style={{ fontSize: "50px" }}
-                            >
-                                {loserData.potSize}
-                            </h1>
-                            <h3
-                                className="LPot__body-text"
-                                style={{ fontWeight: 100, margin: 0 }}
-                            >
-                                times
-                            </h3>
-                        </div>
-                    </div>
-                </div>
+                    }
+                />
             </div>
         </div>
     )
